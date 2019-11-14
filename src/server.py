b039ca194873ef75dacb499b787f44d73710390c
Yanting Wan <yanwan@indiana.edu>
2019-11-14 14:48:27 -0500

    use StopWatch in cloudmesh, test whether I can successfully run StopWatch on docker

--- src/server.py
+++ src/server.py
@@ -1,5 +1,7 @@
 from flask import jsonify
 import connexion
+from cloudmesh.common.StopWatch import StopWatch
+from time import sleep
 
 # Create the application instance
 app = connexion.App(__name__, specification_dir="./")
@@ -11,7 +13,10 @@ app.add_api("api.yaml")
 # create a URL route in our application for "/"
 @app.route("/")
 def home():
-    msg = {"msg": "It's working!"}
+    StopWatch.start("test")
+    sleep(1)
+    StopWatch.stop("test")
+    msg = {"msg": "It's working!", "runtime": StopWatch.get("test")}
     return jsonify(msg)
 
 
